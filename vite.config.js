e06e430ce15fee174418f7c40ef09bd90acfb88a
senbet22 <152603405+senbet22@users.noreply.github.com>
2025-02-27 00:53:20 +0700

    netlify error fix 2:

--- vite.config.js
+++ vite.config.js
@@ -15,10 +15,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "./index.html"),
-        auth: resolve(__dirname, "./auth/index.html"),
-        item: resolve(__dirname, "./item/index.html"),
-        create: resolve(__dirname, "./item/create/index.html"),
-        edit: resolve(__dirname, "./edit/create/index.html"),
+        // auth: resolve(__dirname, "./auth/index.html"),
+        // item: resolve(__dirname, "./item/index.html"),
+        // create: resolve(__dirname, "./item/create/index.html"),
+        // edit: resolve(__dirname, "./edit/create/index.html"),
       },
     },
   },

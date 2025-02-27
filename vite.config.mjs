81c966912496c61a9b5e6862b5927287a2b32c33
senbet22 <152603405+senbet22@users.noreply.github.com>
2025-02-27 20:52:39 +0700

    vite fix**

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,10 @@
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
   appType: "mpa",
-  base: "/",
-  plugins: [tailwindcss()],
+  base: "",
   build: {
     target: "esnext",
     rollupOptions: {
@@ -23,4 +18,5 @@ export default defineConfig({
       },
     },
   },
+  plugins: [tailwindcss()],
 });

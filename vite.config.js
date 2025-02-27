217617b1616f13927a3fe238736d0d3c00d7b3e4
senbet22 <152603405+senbet22@users.noreply.github.com>
2025-02-27 14:04:11 +0700

    netlify fix v8

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  appType: "mpa",
-  base: "/",
+  appType: "mpa", // Keep this if you want to use multiple pages app setup
+  base: "",
   plugins: [tailwindcss()],
   build: {
     target: "esnext",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "./index.html"),
-        auth: resolve(__dirname, "./auth/index.html"),
-        item: resolve(__dirname, "./item/index.html"),
-        profile: resolve(__dirname, "./profile/index.html"),
-        create: resolve(__dirname, "./item/create/index.html"),
-        edit: resolve(__dirname, "./item/edit/index.html"),
+        main: resolve(__dirname, "./index.html"), // Only use the main entry point
       },
     },
   },
